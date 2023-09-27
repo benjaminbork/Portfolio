c3d84f1331c3f240aa78c0b6464112d6e5412116
benjaminbork <benjamin.bork@schifferberg.com>
2023-09-27 09:22:48 +0200

    created protected route

--- netflix-clone/pages/index.tsx
+++ netflix-clone/pages/index.tsx
@@ -1,12 +1,35 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
+import { NextPageContext } from 'next';
+import { getSession, signOut } from 'next-auth/react';
 
-const inter = Inter({ subsets: ['latin'] });
+import useCurrentUser from '@/hooks/useCurrentUser';
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
       <h1 className='text-2xl text-green-500'>Home</h1>
+      <p className='text-white'>Logged in as: {user?.name}</p>
+      <button className='h-10 w-full bg-white' onClick={() => signOut()}>
+        Logout
+      </button>
     </>
   );
 }
