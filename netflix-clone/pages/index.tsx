80282430f88ca09ec9220167b3d3a806255c5cf6
benjaminbork <benjamin.bork@schifferberg.com>
2023-09-27 21:08:46 +0200

    created movie list

--- netflix-clone/pages/index.tsx
+++ netflix-clone/pages/index.tsx
@@ -4,6 +4,8 @@ import { getSession, signOut } from 'next-auth/react';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import Navbar from '@/components/Navbar';
 import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMovieList';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,10 +25,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
+
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className='pb-40'>
+        <MovieList title='Trending now' data={movies} />
+      </div>
     </>
   );
 }
