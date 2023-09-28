dfd320ae3fae4be3f688777139a1d7376ed7f112
benjaminbork <benjamin.bork@schifferberg.com>
2023-09-28 12:05:20 +0200

    fixed missing parameter

--- pages/api/movies/index.ts
+++ pages/api/movies/index.ts
@@ -13,7 +13,7 @@ export default async function handler(
   return res.status(200).json(movies);
 
   try {
-    await serverAuth(req);
+    await serverAuth(req, res);
   } catch (error) {
     console.log(error);
     return res.status(400).end();
